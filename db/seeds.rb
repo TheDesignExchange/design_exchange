243e91af211f14e18e01046c560a988d715c534d
Keien Ohta <keienohta@gmail.com>
2013-10-23 21:09:53 -0700

    Moved rake methods:load to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,72 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+filename = File.join(Rails.root, 'lib/tasks/data/design_methods.xls')
+fields = Hash.new
+
+DesignMethod.destroy_all
+
+data = Spreadsheet.open(filename).worksheet 0
+
+keys = {
+  analy: "Analyzing and Synthesizing Information",
+  build: "Building Prototypes and Mock-ups",
+  commu: "Communicating Within and Across Teams",
+  evalu: "Evaluating and Choosing",
+  gathe: "Gathering Information",
+  gener: "Generating Ideas and Concepts",
+}
+categories = Hash.new
+
+data.each do |row|
+  fields[:name]      = row[1].to_s.strip
+  fields[:overview]  = row[2].to_s.strip
+  fields[:process]   = row[3].to_s.strip
+  fields[:principle] = row[4].to_s.strip
+  # FILL: load citations from spreadsheet
+
+  design_method = DesignMethod.new(fields)
+
+  if !design_method.save
+    p "Error while creating a design method: #{design_method.errors.full_messages}"
+  else
+    p design_method
+  end
+
+  # Read in categories
+
+  string = row[6]
+
+  if string and !string.include?('http') and !string.include?('pg')
+    string.downcase.split(/[\n,]/).each do |cat|  # split by new line character
+      if !cat.blank?
+        cat = cat.strip
+        keys.each do |key, title|
+          if cat.include?(key.to_s)
+            category = MethodCategory.where(name: title).first_or_create!
+            if !design_method.method_categories.include?(category)
+              design_method.method_categories << category
+            end
+            p "#{design_method.name}: #{design_method.method_categories.map {|c| c.name}}"
+          end
+        end
+      end
+    end
+  end
+
+  # citation = row[4]
+  # # not working: somehow the thing being updated in DB is method citations
+  # # and not citations.
+  # if citation #hard to determine what to filter or not.
+  #   citation.split(/[\n\*]/).each do |cit|
+  #     if !cit.blank?
+  #       design_method.citations << Citation.where(text: cit).first_or_create!
+  #       p "#{design_method.name}: #{design_method.citations.map {|c| c.text}}"
+  #     end
+  #   end
+  # end
+end
+
+header = DesignMethod.where(name: "Name").first
+header.destroy if header
